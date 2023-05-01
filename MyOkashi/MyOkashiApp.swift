91593c1b30cead3571e306cacdc41d95abd49c2a
mochidaharuki <mochidaharuki@plise.co.jp>
2023-05-01 14:44:20 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 持田晴生 on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
