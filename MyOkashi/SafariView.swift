ef82d34cbe60f8f3bcaa5ebf58d8e07634f363c0
haruki-mochida <motimotimotida@gmail.com>
2023-12-10 19:29:32 +0900

    SafariViewの作成

---
+++ MyOkashi/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  MyOkashi
+//
+//  Created by 持田晴生 on 2023/12/10.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}
