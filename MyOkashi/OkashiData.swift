b5fbf454d70b5e7975d58d788decc79acefc1f18
haruki-mochida <motimotimotida@gmail.com>
2023-12-10 19:33:41 +0900

    お菓子データを配列に格納

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+//Identifirableプロトコルを利用して、お菓子の情報をまとめる構造体
+struct OkashiItem: Identifiable{
+    let id = UUID()
+    let name: String
+    let link: URL
+    let image: URL
+}
+
  //お菓子データ検索用クラス
 class OkashiData: ObservableObject{
     //JSONのデータ構造
@@ -24,6 +32,11 @@ class OkashiData: ObservableObject{
         let item: [Item]?
     }
     
+    //お菓子のリスト(Identifiableプロトコル)
+    @Published var okashiList:[OkashiItem] = []
+    //クリックサれたWebページのURL情報
+    var okashiLink: URL?
+    
     //WebAPI検索用メソッド　第一引数:keyword 検索したいワード
     func searchOkashi(keyword : String){
         //デバッグエリアに出力
@@ -37,6 +50,10 @@ class OkashiData: ObservableObject{
         }
     }
     
+    //非同期でお菓子データを取得
+    //@Publishedの変数を更新するときはメインスレッドで更新する必要がある
+    //@Mainactorを使いメインスレッドで更新する
+    @MainActor
     private func search(keyword: String)async{
         //お菓子の検索キーワードをURLエンコードする
         guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters:
@@ -60,7 +77,24 @@ class OkashiData: ObservableObject{
             //受け取ったJSONデータをパース(解析)して格納
             let json = try decoder.decode(ResultJson.self, from: data)
             
-            print(json)
+            //お菓子の情報が取得できているか確認
+            guard let items = json.item else { return }
+            //お菓子のリストを取得
+            self.okashiList.removeAll()
+            
+            //取得しているお菓子の数だけ処理
+            for item in items {
+                //お菓子の名称、掲載URL、画像URLをアンラップ
+                if let name = item.name,
+                   let link = item.url,
+                   let image = item.image {
+                    //1つのお菓子を構造体でまとめて管理
+                    let okashi = OkashiItem(name: name,link: link, image: image)
+                    //お菓子の配列へ追加
+                    self.okashiList.append(okashi)
+                }
+            }
+            print(self.okashiList)
             
         }catch{
             //エラー処理
